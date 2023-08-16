cac56fdd8cbcf3899cf3d3d19f2e0e57bbef4fac
PawanKumar <rajiit1212@gmail.com>
2023-08-16 11:33:08 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 
 app.get("/", (req, res) => {
-    res.send("This is the home page.");
+    res.send("This is the home page and i updated it for checking the result.");
 })
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
