d2539500fab311b861daac4a9168c6f6c2c166e5
PawanKumar <rajiit1212@gmail.com>
2023-08-16 10:40:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+app.get("/", (req, res) => {
+    res.send("This is the home page.");
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+})
